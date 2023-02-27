cb4616de4c396a70491a441b8fd7f1dac0c0467c
William Goodson <goodsonwill@gmail.com>
2023-02-27 13:56:06 -0600

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 
-createApp(App).use(router).mount("#app");
+import App from './App.vue'
+import './style.css'
+import Home from "./views/Home.vue"
+import About from "./views/About.vue"
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'Home', component: Home},
+        {path: '/about', name: 'About', component: About}
+    ]
+})
+createApp(App)
+.use(router)
+.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <div class="w-1/2">
+            <h1 class="text-3xl">Home Page</h1>
+        </div>
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
